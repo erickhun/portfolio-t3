7c526c4240ac09f58c36de18ac6536a0f60b1ff7
Eric <enregistrements@gmail.com>
2022-12-01 20:36:41 +0800

    reset

--- src/pages/500.tsx
+++
@@ -1,5 +0,0 @@
-function Custom500() {
-  return (<>500 - Server-side error occurred</>)
-}
-
-export default Custom500;
--- src/pages/templates/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function index() {
-  return <div className="h-full w-full"></div>;
-}
-
-export default index;
